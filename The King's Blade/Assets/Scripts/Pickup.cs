1bbafd9869fad7a79d8d86381017c58290360f70
RobbingBird <Sloofrobbert@gmail.com>
2024-06-03 14:18:58 +0200

    Storage & fake pickup works

--- The King's Blade/Assets/Scripts/Pickup.cs
+++ The King's Blade/Assets/Scripts/Pickup.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     public float pickupRadius = 1f;
+
+    public delegate void onPickupAction();
+    public static event onPickupAction onPickup;
+
     public LayerMask itemLayer;
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             PickupItem();
         }
@@ -24,6 +28,7 @@ public class Pickup : MonoBehaviour
             {
                 Debug.Log("Picked up: " + collider.gameObject.name);
                 Destroy(collider.gameObject);
+                onPickup?.Invoke(); 
                 break;
             }
         }
--- The King's Blade/Assets/Scripts/store.cs
+++ The King's Blade/Assets/Scripts/store.cs
@@ -1,47 +1,89 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 
-public class store : MonoBehaviour
+public class Store : MonoBehaviour
 {
-    //variables
-    public GameObject Steel; 
+    // Public variables for different steel types
+    public GameObject steel; 
     public GameObject orcishSteel;
     public GameObject dwarvenSteel;
     public GameObject elvishSteel;
-    private GameObject steelInstance; 
 
+    // Private instance variable to keep track of the spawned steel
+    private GameObject steelInstance;
+
+    // Configuration variables
     private int steelAmount = 0; 
     public float steelDistance = 0;
     public float steelMax = 4; 
-    public int whatSteel = 0;
+    private int whatSteel = 0;
 
-    private void OnEnable(){
-        Mining.mineSteel += spawnSteel; 
+    // Cached transform component for optimization
+    private Transform cachedTransform;
+
+    private void OnEnable()
+    {
+        Mining.mineSteel += SpawnSteel; 
+        Pickup.onPickup += RemoveSteel;
     }
 
     private void OnDisable()
     {
-        Mining.mineSteel -= spawnSteel;
+        Mining.mineSteel -= SpawnSteel;
+        Pickup.onPickup -= RemoveSteel;
     }
-    void spawnSteel(){
-        if (steelAmount <= steelMax - 1){
-            whatSteel = Random.Range(1,60);
-            if (whatSteel <= 10){
-                steelInstance = Instantiate(elvishSteel, transform);
-            } else if (whatSteel > 10 && whatSteel <= 23){
-                steelInstance = Instantiate(dwarvenSteel, transform);
-            } else if (whatSteel > 23 && whatSteel <= 40){
-                steelInstance = Instantiate(orcishSteel, transform);
-            } else if (whatSteel > 40 && whatSteel <= 60){
-                steelInstance = Instantiate(Steel, transform);
-            }
-            
-            UnityEngine.Vector3 newPosition = transform.position;
-            newPosition.x += steelAmount * steelDistance;
-            steelInstance.transform.position = newPosition;
-            steelAmount += 1;
+
+    private void Start()
+    {
+        cachedTransform = transform;
+    }
+
+    private void SpawnSteel()
+{
+    if (steelAmount < steelMax)
+    {
+        whatSteel = Random.Range(1, 61); // 1 to 60 inclusive
+
+        // Spawn the appropriate type of steel based on random value at the adjusted position
+        if (whatSteel <= 10)
+        {
+            steelInstance = Instantiate(elvishSteel, cachedTransform);
         }
+        else if (whatSteel <= 23)
+        {
+            steelInstance = Instantiate(dwarvenSteel, cachedTransform);
+        }
+        else if (whatSteel <= 40)
+        {
+            steelInstance = Instantiate(orcishSteel, cachedTransform);
+        }
+        else
+        {
+            steelInstance = Instantiate(steel, cachedTransform);
+        }
+
+        steelAmount++;
+
+        Collider2D[] colliders;
+        bool foundSteel = false;
+        do
+        {
+            colliders = Physics2D.OverlapCircleAll(steelInstance.transform.position, 1f); // Adjust the radius as needed
+            foundSteel = false;
+            foreach (Collider2D collider in colliders)
+            {
+                if (collider.CompareTag("Steel") && collider.gameObject != steelInstance)
+                {
+                    steelInstance.transform.position += new Vector3(steelDistance, 0, 0); // Adjust the x position by steelDistance
+                    foundSteel = true;
+                    break;
+                }
+            }
+        } while (foundSteel);
+    }
+}
+
+    private void RemoveSteel()
+    {
+        steelAmount--;
     }
 }
